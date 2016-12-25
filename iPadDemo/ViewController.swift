fed60f66e7a52b0a91aeb9622d3767ce8e6b96e8
leoliuyt <leoliuyt@sina.cn>
2016-12-25 18:16:00 +0800

    commit project

---
+++ iPadDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPadDemo
+//
+//  Created by lbq on 2016/12/22.
+//  Copyright © 2016年 lbq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
